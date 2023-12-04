722db9e6fe31c89f93af08285e643847d3315dd7
FIRST_NAME LAST_NAMEKate <kec22@fsu.edu>
2023-12-04 11:33:32 -0500

    Co-authored-by: Alisha <aag19g@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -1,21 +1,10 @@
-// an example of how to have the position of a face in the webcam's view affect sound
-// it requires chaitaigne, configured to map websockets messages to udp messages, which
-// are received by the pilot synth application (see the included chataigne .noisette config)
-
-// machine learning computer vision stuff came from: https://editor.p5js.org/makerslab/sketches/Cu4TrKwJI
-// websockets stuff came from my demo here: https://github.com/FSUdigitalmedia/p5js_chataigne_demos 
-
-// (note that, unlike all of the other p5js code so far, this one gets all of the required 
-//  javascript libraries from the internet, instead of a libraries folder - look at index.html)
-
 var net, video, currentResult, img;
 var note = 0;
-var duration = 16; // full measure
-// var drawingNose;
+var duration = 16; 
 var nosePath = [];
 
-let host = '127.0.0.1:8080'; // address of the websockets server
-let socket; // the websocket connection
+let host = '192.168.0.198:8080'; 
+let socket; 
 
 function setup() {
     createCanvas(800, 600);
@@ -25,7 +14,6 @@ function setup() {
     video.size(800, 600);
     video.hide();
 
-    // connect to server...
     socket = new WebSocket('ws://' + host);
     socket.onopen = openHandler;
 }
@@ -35,14 +23,7 @@ function draw() {
     image(video, 0, 0, 800, 600);
 
     if (currentResult) {
-        // got your nose! :*) 
         var nose = currentResult.keypoints[0].position;
-
-        // if(drawingNose){
-        //     stroke(0);
-        //     strokeWeight(4);
-        //     line(pmouseX, pmouseY, mouseX, mouseY);
-        // } 
 
         nosePath.push(createVector(nose.x, nose.y))
 
@@ -59,22 +40,17 @@ function draw() {
         } endShape();
 
 
-        // ellipse(nose.x, nose.y, 50); // draw a nose
-
         note = map(nose.x, 0, width, 0, 25); // note is 0-25 based on left-right position of nose
         duration = map(nose.y, 0, height, 0, 16); // note length (0-16) based on up-down nose position
-
-        // drawingNose = createVector(nose.x, nose.y);
     }
 }
 
-// send a note message every time a key is pressed
 function keyPressed() {
     sendNote(int(note), int(duration));
 }
 
 // send the note message to the ws server
-function sendNote(noteNumber, noteLength) {
+function sendNote(noteNumber, noteLength) { console.log("keyPressed")
     
     // don't continue if the numbers are weird
     if (noteNumber < 0 || noteNumber > 25 || noteLength < 0 || noteLength > 16) {
